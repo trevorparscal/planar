76a72319a36a206664fd6c4ad375b024415a2b67
Trevor Parscal <trevorparscal@gmail.com>
2017-03-19 17:50:19 -0400

    Fix shape changing in Animation

--- src/systems/Animation.js
+++ src/systems/Animation.js
@@ -41,16 +41,23 @@ Planar.System.Animation = class extends Planar.System {
 	update( delta ) {
 		for ( let entity of this.entities ) {
 			let tween = this.tweens.get( entity.key );
-			tween.value += delta;
+			tween.value++;
 			if ( tween.value % 100 === 0 ) {
-				let index = Math.round( Math.random() * 2 );
-				entity.change( { shape: { type: ['rectangle', 'circle', 'ngon'][index] } } );
+				let index = Math.round( Math.random() * 3 ),
+					size = 32,
+					hash = entity.components.shape.hash;
+				entity.change( { shape: [
+					{ type: 'rectangle', width: size, height: size }, 
+					{ type: 'circle', radius: size / 2 },
+					{ type: 'ngon', radius: size / 2, sides: 3 },
+					{ type: 'ngon', radius: size / 2, sides: 5 }
+				][index] } );
 			}
 			entity.change( {
 				warp: {
 					scale: new Planar.Point(
-						1.5 + ( Math.sin( tween.value * ( Math.PI / 1000 ) ) * 0.5 ),
-						1.5 + ( Math.cos( tween.value * ( Math.PI / 1000 ) ) * 0.5 )
+						1.5 + ( Math.sin( tween.value * ( Math.PI / 100 ) ) * 0.5 ),
+						1.5 + ( Math.cos( tween.value * ( Math.PI / 100 ) ) * 0.5 )
 					)
 				}
 			} );
