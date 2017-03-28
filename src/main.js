eddbf3f14482f16b7dbb083159ed035f5af30bec
Trevor Parscal <trevorparscal@gmail.com>
2017-03-28 03:08:14 -0400

    Add getJSON global utility function

--- src/main.js
+++ src/main.js
@@ -5,5 +5,21 @@
  * @type {Object}
  */
 window.Planar = {
-	debug: true
+	debug: true,
+	getJSON: function( url ) {
+		return new Promise( function ( resolve, reject ) {
+			var xhr = new XMLHttpRequest();
+			xhr.open( 'GET', url, true );
+			xhr.responseType = 'json';
+			xhr.onload = function () {
+				var status = xhr.status;
+				if ( status == 200 ) {
+					resolve( xhr.response );
+				} else {
+					reject( status );
+				}
+			};
+			xhr.send();
+		} );
+	}
 };
